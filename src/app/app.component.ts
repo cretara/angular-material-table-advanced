399bfabc6757180ba56be68683bb97179dd08735
Pier Daniele Cretara <pierdanielecretara@lviiier.com>
2023-01-26 14:04:53 +0100

    Get coments by public open JSON API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, of, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-material-table-advanced';
+export class AppComponent implements OnInit, OnDestroy {
+
+  public commentList: Observable<Comment[]> = of([]);
+  displayedColumns: string[] = ["id", "postId", "name", "email", "body"];
+  private commentListSubscription: Subscription = new Subscription();
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.commentList = this.httpClient
+      .get<Comment[]>("https://jsonplaceholder.typicode.com/comments")
+    this.commentListSubscription = this.commentList.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.commentListSubscription && this.commentListSubscription.unsubscribe();
+  }
+
 }
