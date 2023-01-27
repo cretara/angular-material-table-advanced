554014653ba248a85f6a31da4547d4d1bc037eba
Pier Daniele Cretara <pierdanielecretara@lviiier.com>
2023-01-27 23:56:47 +0100

    Implmented mat-table filter on input as fromEvent observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,53 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, of} from "rxjs";
+import {Comment} from "src/app/model/comment";
+import {debounceTime, distinctUntilChanged, fromEvent, map, Observable, Subscription} from "rxjs";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  public commentList: Observable<Comment[]> = of([]);
+  @ViewChild("nameFilter")
+  public nameFilterInput!: ElementRef;
+
   displayedColumns: string[] = ["id", "postId", "name", "email", "body"];
+
+  commentListDataSource: MatTableDataSource<Comment> = new MatTableDataSource<Comment>();
+
+  private nameFilterInputObservable!: Observable<Event>;
+  private nameFilterInputSubscription: Subscription = new Subscription();
 
   constructor(private httpClient: HttpClient) {
   }
 
-  ngOnInit(): void {
-    this.commentList = this.httpClient
-      .get<Comment[]>("https://jsonplaceholder.typicode.com/comments")
-  }
-
   ngOnDestroy(): void {
+    this.nameFilterInputSubscription && this.nameFilterInputSubscription.unsubscribe();
   }
 
+  ngAfterViewInit(): void {
+    this.nameFilterInputObservable = fromEvent((this.nameFilterInput.nativeElement as HTMLInputElement), "keyup");
+    this.nameFilterInputSubscription = this.nameFilterInputObservable
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(300),
+        map((event) => (event.target as HTMLInputElement).value)
+      )
+      .subscribe((nameFilterValue) => {
+        console.debug("filter next", nameFilterValue);
+        nameFilterValue = nameFilterValue.trim();
+        nameFilterValue = nameFilterValue.toLowerCase();
+        this.commentListDataSource.filter = nameFilterValue;
+      })
+  }
+
+  ngOnInit(): void {
+    this.httpClient
+      .get<Comment[]>("https://jsonplaceholder.typicode.com/comments")
+      .subscribe((commentListObservableValue) =>
+        this.commentListDataSource.data = commentListObservableValue)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import {AppComponent} from './app.component';
 import {MatTableModule} from "@angular/material/table";
 import {HttpClientModule} from "@angular/common/http";
 import {MatTableFilterModule} from "mat-table-filter";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    MatTableModule,
-    HttpClientModule,
-    MatTableFilterModule
-  ],
+    imports: [
+        BrowserModule,
+        MatTableModule,
+        HttpClientModule,
+        MatTableFilterModule,
+        MatInputModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
